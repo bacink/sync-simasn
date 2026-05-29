cf46f3167578b489cdbf5ab00da8d12c3762b4da
bacink <bacink666@gmail.com>
2026-05-29 11:48:35 +0700

    feat: add SIM-ASN OAuth routes to web.php

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SimAsn\ArchiveSyncController;
+use App\Http\Controllers\SimAsnCallbackController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,9 @@ Route::get('/', function () {
 
 Route::get('/sim-asn/archive-sync', [ArchiveSyncController::class, 'index'])
     ->name('sim-asn.archive-sync.index');
+
+// SIM-ASN OAuth
+Route::get('/auth/sim-asn', [SimAsnCallbackController::class, 'initiate'])
+    ->name('auth.sim-asn');
+Route::get('/callback/sim-asn', [SimAsnCallbackController::class, 'callback'])
+    ->name('callback.sim-asn');
